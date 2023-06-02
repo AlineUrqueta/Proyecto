da840d3d32dae7123a7c5e821670bc47381b9239
AlineUrqueta <Aline.urqueta@gmail.com>
2023-06-02 19:31:09 -0400

    Index Estudiante

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Estudiante;
 
 class EstudianteController extends Controller
 {
-    //
+    public function show(Estudiante $estudiante){
+        return view('estudiantes.show',compact('estudiante'));
+    }
+
+    public function index(){
+        $estudiantes = Estudiante::all();
+        return view('estudiantes.index',compact('estudiantes'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EstudianteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/estudiantes',[EstudianteController::class,'index'])->name('estudiantes.index');
+Route::get('/estudiantes/{estudiante}',[EstudianteController::class,'show'])->name('estudiantes.show');
