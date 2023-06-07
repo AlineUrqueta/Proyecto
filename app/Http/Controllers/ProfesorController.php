86ddaa08c39d711e38d989f7da26bbe3fb4cc548
lvlMiria <m.pilar.bruna@gmail.com>
2023-06-06 23:31:43 -0400

    revisando profesor pt2

--- app/Http/Controllers/ProfesorController.php
+++ app/Http/Controllers/ProfesorController.php
@@ -11,4 +11,19 @@ class ProfesorController extends Controller
         return view('profesores.index');
     }
 
+    public function comment(){
+        return view('profesores.comment');
+    }
+
+    public function guardarComentario(Request $request){
+        $comentario = $request->input('comentario');
+        
+        // Guardar el comentario en la base de datos
+        Profesor_Propuesta::create([
+            'comentario' => $comentario
+        ]);
+        
+        // Redireccionar a la página de comentarios exitosamente guardado
+        return redirect()->route('profesores.index')->with('success', 'Comentario guardado exitosamente');
+    }
 }
