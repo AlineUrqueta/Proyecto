c62ee6ee525f8358f6c80bdd572e91884663a6bc
lvlMiria <m.pilar.bruna@gmail.com>
2023-06-06 22:47:59 -0400

    preparando profe pt3

--- app/Http/Controllers/ProfesorController.php
+++ app/Http/Controllers/ProfesorController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Profesor_Propuesta;
 
 class ProfesorController extends Controller
 {
-    //
+    public function index(){
+        return view('profesores.index');
+    }
+    public function comment(){
+        return view('profesores.comment');
+    }
+    public function guardarComentario(Request $request)
+    {
+        $comentario = $request->input('comentario');
+        
+        // Guardar el comentario en la base de datos
+        Profesor_Propuesta::create([
+            'comentario' => $comentario
+        ]);
+        
+        // Redireccionar a la página de comentarios exitosamente guardado
+        return redirect()->route('profesores.index')->with('success', 'Comentario guardado exitosamente');
+    }
+
 }
